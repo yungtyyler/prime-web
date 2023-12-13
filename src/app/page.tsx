fe05c849d344cdac7125ece7d1c933ad31d13a27
Tyler Varzeas <tyler.varzeas@gmail.com>
2023-12-12 18:38:45 -0800

    Finish carousel component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,49 @@
 import { Hero, ServicesCarousel } from '@/components';
+import Slide from '@/types/slide';
 
 export default function Home() {
+  const slides: Slide[] = [
+    {
+      id: 0,
+      title: 'Internal Apps',
+      image: '/carousel/internal-apps.jpg',
+      description:
+        'Internal apps are apps that are used by your employees to help them do their jobs better and faster. They can be used to automate repetitive tasks, manage data, and more.',
+    },
+    {
+      id: 1,
+      title: 'Web Development',
+      image: '/carousel/web-development.jpg',
+      description:
+        'Web development is the process of building websites and applications for the internet. It includes aspects such as web design, web publishing, web programming, and database management.',
+    },
+    {
+      id: 2,
+      title: 'Search Engine Optimization',
+      image: '/carousel/seo.jpg',
+      description:
+        'Search engine optimization is the process of improving the visibility of a website or a web page in search engines. It includes aspects such as keyword research, on-page optimization, and link building.',
+    },
+    {
+      id: 3,
+      title: 'Consulting',
+      image: '/carousel/consulting.jpg',
+      description:
+        'Consulting is the process of providing expert advice to a client. It includes aspects such as consulting, consulting, and consulting.',
+    },
+  ];
+
   return (
     <section>
       <Hero />
       <div className="page__container">
-        <div className="my-4 flex flex-col items-center">
+        <div className="my-4 flex flex-col items-center gap-8">
           <h1 className="uppercase text-prime-purple-700 font-extrabold text-4xl">
             What we can do for you
           </h1>
-          <ServicesCarousel />
+          <div className="max-w-3xl w-full mx-auto h-[450px]">
+            <ServicesCarousel slides={slides} />
+          </div>
         </div>
       </div>
     </section>
--- src/components/sections/ServicesCarousel.tsx
+++ src/components/sections/ServicesCarousel.tsx
@@ -1,29 +1,58 @@
-import React from 'react';
-import Slide from '@/types/slide';
-import Image from 'next/image';
+'use client';
 
-const ServicesCarousel = () => {
-  const slides: Slide[] = [
-    { id: 0, title: 'Internal Apps', image: '/carousel/internal-apps.jpg', description: 'Internal apps are apps that are used by your employees to help them do their jobs better and faster. They can be used to automate repetitive tasks, manage data, and more.' },
-    { id: 1, title: 'Web Development', image: '/carousel/web-development.jpg', description: 'Web development is the process of building websites and applications for the internet. It includes aspects such as web design, web publishing, web programming, and database management.' },
-  ];
+import { useState } from 'react';
+import Slide from '@/types/slide';
+
+const ServicesCarousel = ({ slides }: { slides: Slide[] }) => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const prevSlide = () => {
+    const isFirstSlide = currentSlide === 0;
+    const newSlide = isFirstSlide ? slides.length - 1 : currentSlide - 1;
+    console.log(newSlide);
+    setCurrentSlide(newSlide);
+  };
+
+  const nextSlide = () => {
+    const isLastSlide = currentSlide === slides.length - 1;
+    const newSlide = isLastSlide ? 0 : currentSlide + 1;
+    console.log(newSlide);
+    setCurrentSlide(newSlide);
+  };
+
+  const goToSlide = (id: number) => {
+    setCurrentSlide(id);
+  };
 
   return (
-    <div className="md:max-w-3xl z-0 w-full flex overflow-hidden">
-      {slides.map((slide, index) => (
-        <div
-          className={`w-full h-full relative transition-transform duration-500 ${
-            index === 0 ? 'transform translate-x-0' : 'transform translate-x-full'
-          }`}
-          key={slide.id}
-        >
-          <Image src={slide.image} alt={slide.title} width={500} height={300} className='w-full h-full object-cover' />
-          <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center text-center bg-black bg-opacity-50 text-white p-4">
-            <h2 className="text-2xl font-bold">{slide.title}</h2>
-            <p className="text-gray-500">{slide.description}</p>
+    <div className="h-full relative flex flex-col items-center">
+      <div className="absolute top-2 px-12 my-4 bg-white bg-opacity-75 rounded text-[1.5em] tracking-wider uppercase font-bold">
+        {slides[currentSlide].title}
+      </div>
+      <div className="absolute bottom-10 w-[80%] px-10 my-4 bg-black bg-opacity-80 text-white text-[1.1em] rounded">
+        {slides[currentSlide].description}
+      </div>
+      <div className="leftArrow" onClick={prevSlide}>
+        ❰
+      </div>
+      <div className="rightArrow" onClick={nextSlide}>
+        ❱
+      </div>
+      <div
+        style={{ backgroundImage: `url(${slides[currentSlide].image})` }}
+        className="w-full h-full rounded bg-center bg-cover"
+      />
+      <div className="text-black flex justify-center">
+        {slides.map((slide) => (
+          <div
+            key={slide.id}
+            className="mx-1 cursor-pointer text-[20px]"
+            onClick={() => goToSlide(slide.id)}
+          >
+            &#11044;
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
