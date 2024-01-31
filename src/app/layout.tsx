b07c6a5e6dc9ee0a4c632b38808c0fe9cc43dadd
Tyler Varzeas <tyler.varzeas@gmail.com>
2024-01-30 18:24:35 -0800

    Update description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,7 @@ import { roboto } from '@/fonts/fonts';
 
 export const metadata: Metadata = {
   title: 'Prime Web Solutions',
-  description:
-    'Prime Web Solutions is a software development company dedicated to building high quality web applications to help grow businesses through meaningful digital experiences.',
+  description: `Prime Web Solutions, a software development company, is committed to crafting top-tier web applications that elevate businesses through impactful digital interactions. Our team specializes in delivering high-quality custom websites and internal applications tailored to fuel growth and success. Explore our innovative approach to software and web development, designed to provide businesses with the competitive edge they need in today's dynamic digital landscape. Elevate your online presence with Prime Web Solutions - where excellence meets technology to redefine the digital experience for your business.`,
 };
 
 export default function RootLayout({
